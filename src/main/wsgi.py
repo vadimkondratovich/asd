a08b11c4c52182bb999734835f9dbc895521f320
Your Name <you@example.com>
2020-12-23 22:34:15 +0300

    xxx

--- src/main/wsgi.py
+++ src/main/wsgi.py
@@ -23,7 +23,7 @@ def application(environ, start_response):
         b'<meta charset="utf-8">'
         b"</head>"
         b"<body>"
-        b"<h1>Project Alpha</h1>"
+        b"<h1>Project XXXX</h1>"
         b"<hr>"
         b"<p>This is a template project.</p>"
         b"</body>"
