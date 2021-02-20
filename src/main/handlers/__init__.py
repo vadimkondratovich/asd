48a3978dcd24cf46e81c39b4f3dc79825e61e201
vadimkondratovich <vadim.kondratovich1993@gmail.com>
2021-02-20 18:36:45 +0300

    lesson_django3

--- src/main/handlers/__init__.py
+++ src/main/handlers/__init__.py
@@ -6,7 +6,17 @@ from tasks.lesson01 import task103
 from tasks.lesson03 import task301
 from tasks.lesson03 import task302
 from tasks.lesson03 import task303
+from tasks.lesson03 import task304
+from tasks.lesson03 import task305
+from tasks.lesson03 import task306
+from tasks.lesson03 import task307
 from tasks.lesson03 import task309
+from tasks.lesson03 import task310
+from tasks.lesson03 import task311
+
+
+from tasks.lesson04 import task402
+
 
 from . import error_test
 from . import index
@@ -20,7 +30,14 @@ urlpatterns: Dict[str, HandlerT] = {
     "/tasks/3/301/": task301.handler,
     "/tasks/3/302/": task302.handler,
     "/tasks/3/303/": task303.handler,
+    "/tasks/3/304/": task304.handler,
+    "/tasks/3/305/": task305.handler,
+    "/tasks/3/306/": task306.handler,
+    "/tasks/3/307/": task307.handler,
     "/tasks/3/309/": task309.handler,
+    "/tasks/3/310/": task310.handler,
+    "/tasks/3/311/": task311.handler,
+    "/tasks/4/402/": task402.handler,
 }
 
 
--- src/tasks/lesson03/domzad_four.py
+++ src/tasks/lesson03/domzad_four.py
@@ -13,9 +13,11 @@ def ask_user_to_input_a_number() -> Number:
     a = float(input('Введите целое число: a = '))
     return Number(a)
 
+
 def what_is_A(a) -> float:
     k = isinstance(a, (int, float))
     return k
+
 
 class Exp(NamedTuple):
     e: float
@@ -26,12 +28,13 @@ def solution(x: Number) -> Exp:
     Multiplies a number three times.
     """
     e = float(expo(x.a))
-    return Exp(e = e)
+    return Exp(e=e)
 
 
 def expo(a) -> float:
     e = float(a ** 3)
     return e
+
 
 if __name__ == "__main__":
     C = ask_user_to_input_a_number()
